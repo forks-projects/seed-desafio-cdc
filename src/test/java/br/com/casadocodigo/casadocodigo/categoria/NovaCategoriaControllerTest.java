b56bf18c48eea584aaefc5f00bb7f7e9b91e52b4
Adriano Avelino <adriano-st777@hotmail.com>
2025-01-12 14:08:54 -0300

    adicoina builder para criação de nova categoria request

--- src/test/java/br/com/casadocodigo/casadocodigo/categoria/NovaCategoriaControllerTest.java
+++ src/test/java/br/com/casadocodigo/casadocodigo/categoria/NovaCategoriaControllerTest.java
@@ -1,6 +1,7 @@
 package br.com.casadocodigo.casadocodigo.categoria;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class NovaCategoriaControllerTest {
 
     @Autowired
@@ -28,7 +31,7 @@ class NovaCategoriaControllerTest {
     @Test
     @DisplayName("Deve cadastrar uma categoria com sucesso")
     void deveCadastrarCategoriaComSucesso() throws Exception {
-        NovaCategoriaRequest novaCategoriaRequest = new NovaCategoriaRequest("Ficção");
+        NovaCategoriaRequest novaCategoriaRequest = NovaCategoriaRequestDataBuilder.umaCategoria().build();
 
         mockMvc.perform(post("/v1/categorias")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -39,10 +42,11 @@ class NovaCategoriaControllerTest {
     @Test
     @DisplayName("Deve retornar erro 409 quando a categoria já existir")
     void deveRetornarErroSeCategoriaJaExistir() throws Exception {
-        Categoria categoriaExistente = new Categoria("Drama");
-        categoriaRepository.save(categoriaExistente);
+        NovaCategoriaRequest novaCategoriaRequest = NovaCategoriaRequestDataBuilder
+                .umaCategoria().comNome("Drama").build();
 
-        NovaCategoriaRequest novaCategoriaRequest = new NovaCategoriaRequest("Drama");
+        Categoria categoriaExistente = new Categoria(novaCategoriaRequest.getNome());
+        categoriaRepository.save(categoriaExistente);
 
         mockMvc.perform(post("/v1/categorias")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -51,13 +55,68 @@ class NovaCategoriaControllerTest {
     }
 
     @Test
-    @DisplayName("Deve retornar erro 400 quando o nome da categoria for inválido")
-    void deveRetornarErroSeNomeForInvalido() throws Exception {
-        NovaCategoriaRequest novaCategoriaRequest = new NovaCategoriaRequest(""); 
+    @DisplayName("Deve retornar mensagem de erro quando categoria já existir")
+    void deveRetornarMensagemDeErroQuandoCategoriaJaExistir() throws Exception {
+        NovaCategoriaRequest novaCategoriaRequest = NovaCategoriaRequestDataBuilder
+                .umaCategoria().comNome("Drama").build();
+
+        Categoria categoriaExistente = new Categoria(novaCategoriaRequest.getNome());
+        categoriaRepository.save(categoriaExistente);
+
+        mockMvc.perform(post("/v1/categorias")
+                .header("Accept-Language", "pt-BR")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(novaCategoriaRequest)))
+                .andExpect(jsonPath("$.status").value(409))
+                .andExpect(jsonPath("$.erro").value("Categoria já está cadastrada"));
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro 400 quando o nome da categoria for em branco")
+    void deveRetornarErroSeNomeForEmBranco() throws Exception {
+        NovaCategoriaRequest novaCategoriaRequest = NovaCategoriaRequestDataBuilder.umaCategoriaComNomeEmBranco().build();
 
         mockMvc.perform(post("/v1/categorias")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(novaCategoriaRequest)))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Deve retornar mensagem de erro quando o nome for em branco")
+    void deveRetornarMensagemDeErroQuandoNomeForEmBranco() throws Exception {
+        NovaCategoriaRequest novaCategoriaRequest = NovaCategoriaRequestDataBuilder.umaCategoriaComNomeEmBranco().build();
+
+        mockMvc.perform(post("/v1/categorias")
+                .header("Accept-Language", "pt-BR")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(novaCategoriaRequest)))
+                .andExpect(jsonPath("$.listaErros[0].campo").value("nome"))
+                .andExpect(jsonPath("$.listaErros[0].erro").value("não deve estar em branco"));
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro 400 quando o nome da categoria for nulo")
+    void deveRetornarErroSeNomeForNulo() throws Exception {
+        NovaCategoriaRequest novaCategoriaRequest = NovaCategoriaRequestDataBuilder.umaCategoriaComNomeNulo().build();
+
+        mockMvc.perform(post("/v1/categorias")
+                .header("Accept-Language", "pt-BR")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(novaCategoriaRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Deve retornar mensagem de erro quando o nome for nulo")
+    void deveRetornarMensagemDeErroQuandoNomeForNulo() throws Exception {
+        NovaCategoriaRequest novaCategoriaRequest = NovaCategoriaRequestDataBuilder.umaCategoriaComNomeNulo().build();
+
+        mockMvc.perform(post("/v1/categorias")
+                .header("Accept-Language", "pt-BR")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(novaCategoriaRequest)))
+                .andExpect(jsonPath("$.listaErros[0].campo").value("nome"))
+                .andExpect(jsonPath("$.listaErros[0].erro").value("não deve estar em branco"));
     }
 }
---
+++ src/test/java/br/com/casadocodigo/casadocodigo/categoria/NovaCategoriaRequestDataBuilder.java
@@ -0,0 +1,29 @@
+package br.com.casadocodigo.casadocodigo.categoria;
+
+public class NovaCategoriaRequestDataBuilder {
+    private String nome = "Categoria Padrão";
+    private NovaCategoriaRequest novaCategoriaRequest;
+
+    public NovaCategoriaRequestDataBuilder() {
+        this.novaCategoriaRequest = new NovaCategoriaRequest(nome);
+    }
+    public NovaCategoriaRequestDataBuilder comNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+    public NovaCategoriaRequest build() {
+        return new NovaCategoriaRequest(nome);
+    }
+
+    static public NovaCategoriaRequestDataBuilder umaCategoria() {
+        return new NovaCategoriaRequestDataBuilder();
+    }
+
+    static public NovaCategoriaRequestDataBuilder umaCategoriaComNomeEmBranco() {
+      return new NovaCategoriaRequestDataBuilder().comNome("");
+    }
+
+    static public NovaCategoriaRequestDataBuilder umaCategoriaComNomeNulo() {
+      return new NovaCategoriaRequestDataBuilder().comNome(null);
+    }
+}
